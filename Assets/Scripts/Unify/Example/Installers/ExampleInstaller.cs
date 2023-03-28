efd4794f1839c6dab643b4dcfb7c72afa74844a6
Kamiel de Visser <kamieldevisser@gmail.com>
2023-03-28 22:03:26 +0200

    fix example

--- Assets/Scripts/Unify/Example/Installers/ExampleInstaller.cs
+++ Assets/Scripts/Unify/Example/Installers/ExampleInstaller.cs
@@ -1,6 +1,5 @@
 ﻿using Unify.Core.Installers;
 using Unify.Example.Behaviours;
-using Unify.Example.Factories;
 using UnityEngine;
 
 namespace Unify.Example.Installers
@@ -12,17 +11,14 @@ namespace Unify.Example.Installers
         public override void RegisterDependencies()
         {
             // Register a string instance from the inspector
-            using (var builder = LocalContainer.RegisterDependency<string>().FromInstance(someStringDependency))
-            {
-                
-            }
-            
+            LocalContainer.RegisterDependency<string>().FromInstance(someStringDependency);
+
             // Register a FooBehaviour instance from the inspector with id "fromHierarchy"
             LocalContainer.RegisterDependency<FooBehaviour>().FromInstance(fooBehaviourInHierarchy).WithId("fromHierarchy");
 
             // Create FooBehaviour from installer and register it with id "fromCode" 
             var fooBehaviourFromCode = new GameObject("FooBehaviour From Code").AddComponent<FooBehaviour>();
-            LocalContainer.RegisterDependency<FooBehaviour>().FromInstance(fooBehaviourInHierarchy).WithId("fromCode");
+            LocalContainer.RegisterDependency<FooBehaviour>().FromInstance(fooBehaviourFromCode).WithId("fromCode");
             
             // Create a BarBehaviour that has it's own dependencies
             var barBehaviour = new GameObject("BarBehaviourWithDependency").AddComponent<BarBehaviour>();
