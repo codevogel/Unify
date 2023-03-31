bd08fd35e23e06d6474e8a23d5cb64500d067ab7
Kamiel de Visser <kamieldevisser@gmail.com>
2023-03-31 14:31:38 +0200

    pull up builder methods

--- Assets/Plugins/Unify/Core/Builders/DependencyBuilder/IDependencyBuilder.cs
+++ Assets/Plugins/Unify/Core/Builders/DependencyBuilder/IDependencyBuilder.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Plugins.Unify.Core.Builders.DependencyBuilder
 {
     public interface IDependencyBuilder<in TDependency> : IDisposable
     {
-        public IDependencyBuilder<TDependency> FromInstance(TDependency instance);
+        public IDependencyBuilder<TDependency> FromComponentOnNewGameObject(string name = default);
+        public IDependencyBuilder<TDependency> FromPrefab(GameObject gameObjectWithDependencyAttached);
 
+        public IDependencyBuilder<TDependency> FromInstance(TDependency instance);
+        public IDependencyBuilder<TDependency> FromSubstitute();
         public IDependencyBuilder<TDependency> WithId(string id);
-        void Register();
+        public IDependencyBuilder<TDependency> AsInterfaceTo<TConcrete>();
+
+        public void Register();
+
     }
 }
